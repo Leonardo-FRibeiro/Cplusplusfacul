d431bfdb9042c49d5083692bddf0fdf00720e47e
Oliveira04 <augustocoimbra01@hotmail.com>
2023-08-21 11:08:51 -0300

    main

--- Faculdade/Trabalho Freecell/pilha.cpp
+++ Faculdade/Trabalho Freecell/pilha.cpp
@@ -2,11 +2,11 @@
 #include "carta.h"
 
 
-Stack::Stack(){ //Ação que indica o valor inicial da Pilha
+Pilha::Pilha(){ //Ação que indica o valor inicial da Pilha
     top = 0;
 }
 
-void Stack::Push(){ //Ação que verifica se a pilha não esta cheia ou se possui um lixo de momoria
+void Pilha::Push(){ //Ação que verifica se a pilha não esta cheia ou se possui um lixo de momoria
     if(Full()){
         cout << "Pilha cheia" << endl;
         abort();
@@ -18,12 +18,12 @@ void Stack::Push(){ //Ação que verifica se a pilha não esta cheia ou se possu
 
 }
 
-void Carta::Inicializar(int val, string naip){
+void Pilha::Inicializar(int val, string naip){
     
 }
 
 
 
-Stack_Saida::Stack_Saida(){
+Pilha_Saida::Pilha_Saida(){
     top_saida = 0;
 }
--- Faculdade/Trabalho Freecell/pilha.h
+++ Faculdade/Trabalho Freecell/pilha.h
@@ -1,21 +1,27 @@
 #include "carta.cpp"
 const int MAXSTACK = 26;
-class Stack{
+
+class carta; //Chama o metodo da Classe Carta
+class Pilha{
     public:
-    Stack();
+    Pilha();
     void Push();
     void Pop();
     void Full();
+    void Inicializar(int val, string naip); //Com o chamado da Classe Carta, pode se usar os parametros de carta, dentro da pilha 
+    void Display();
+
+
     private:
     int top;
     int Entry[MAXSTACK+1];
 };
 
 
-class Stack_Saida{
+class Pilha_Saida{
     public:
     
-    Stack_Saida();
+    Pilha_Saida();
     void Push_Saida();
     void Pop_Saida();
     void Full_Saida();
